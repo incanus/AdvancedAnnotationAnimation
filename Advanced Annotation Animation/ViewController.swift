d2217d10862779251590e6713761a4481b02942d
Justin R. Miller <incanus@codesorcery.net>
2014-11-20 16:25:26 -0800

    initial commit

--- Advanced Annotation Animation/ViewController.swift
+++ Advanced Annotation Animation/ViewController.swift
@@ -1,25 +1,80 @@
-//
-//  ViewController.swift
-//  Advanced Annotation Animation
-//
-//  Created by Justin R. Miller on 11/20/14.
-//  Copyright (c) 2014 Mapbox. All rights reserved.
-//
-
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let map = MKMapView(frame: view.bounds)
+        map.autoresizingMask = .FlexibleWidth | .FlexibleHeight
+        map.delegate = self
+        view.addSubview(map)
+
+        for i in 0...20 {
+            let deltaRandom = Double(arc4random() % 20) * 0.001
+
+            var latRandom = arc4random() % 2
+            var lonRandom = arc4random() % 2
+
+            let latDelta = deltaRandom * (latRandom == 1 ? -1 : 1)
+            let lonDelta = deltaRandom * (lonRandom == 1 ? -1 : 1)
+
+            let point = MKPointAnnotation()
+            point.coordinate = CLLocationCoordinate2D(latitude: 37.783333 + latDelta, longitude: -122.416667 + lonDelta)
+            map.addAnnotation(point)
+        }
+
+        map.setRegion(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.783333, longitude: -122.416667), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)), animated: false)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+        return AnnotationView()
+    }
+}
+
+class AnnotationView: MKAnnotationView {
+
+    var sizeRandom: Int?
+    var colorRandom: Int?
+    var durationRandom: Int?
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
     }
 
+    override convenience init() {
+        let size = Int(arc4random() % 20)
+
+        self.init(frame: CGRect(x: 0, y: 0, width: 20 + size, height: 20 + size))
+
+        sizeRandom     = size
+        colorRandom    = Int(arc4random() % 5)
+        durationRandom = Int(arc4random() % 5) + 1
+
+        let view = SSPieProgressView(frame: frame)
+
+        let colors = [ UIColor.redColor(), UIColor.blackColor(), UIColor.greenColor(), UIColor.blueColor(), UIColor.purpleColor(), UIColor.orangeColor() ]
+
+        let color = colors[colorRandom!]
+
+        view.pieBorderColor = color
+        view.pieFillColor   = color
+        view.pieBackgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.5)
+
+        view.progress = 0.33
+
+        self.addSubview(view)
+
+        UIView.animateWithDuration(NSTimeInterval(durationRandom!), delay: 0, options: .Repeat | .Autoreverse, animations: {
+            view.transform = CGAffineTransformMakeRotation(CGFloat(1.5 * M_PI))
+        }, completion: nil)
+
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
 }
 
